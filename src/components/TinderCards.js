e2af25c03a363d599ca1d378c1e05d1a2f96bb88
Charles BIDAUT <bidaut.charles@gmail.com>
2020-11-15 18:35:25 +0100

    feat(*): add material grid + create basic profil / msg / login page

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import TinderCard from 'react-tinder-card';
 import axios from 'utils/axios';
-
 import 'style/TinderCards.css';
+import Grid from '@material-ui/core/Grid';
 
 function TinderCards() {
   const [people, setPeople] = useState([]);
@@ -27,24 +27,24 @@ function TinderCards() {
   };
 
   return (
-    <div className="tinderCards">
-      <div className="tinderCards__cardContainer">
-        {people.map((person) => (
-          <TinderCard
-            className="swipe"
-            key={person.name}
-            preventSwipe={['up', 'down']}
-            onSwipe={(dir) => swiped(dir, person.name)}
-            onCardLeftScreen={() => outOfFrame(person.name)}
+    <Grid item container justify="center">
+      {people.map((person) => (
+        <TinderCard
+          className="swipe"
+          key={person.name}
+          preventSwipe={['up', 'down']}
+          onSwipe={(dir) => swiped(dir, person.name)}
+          onCardLeftScreen={() => outOfFrame(person.name)}
+        >
+          <div
+            style={{ backgroundImage: `url(${person.imgUrl})` }}
+            className="card"
           >
-            <div style={{ backgroundImage: `url(${person.imgUrl})` }} className="card">
-              <h3>{person.name}</h3>
-            </div>
-          </TinderCard>
-        ))}
-
-      </div>
-    </div>
+            <h3>{person.name}</h3>
+          </div>
+        </TinderCard>
+      ))}
+    </Grid>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Header from 'components/Header';
 import TinderCards from 'components/TinderCards';
 import SwipeButtons from 'components/SwipeButtons';
+import Grid from '@material-ui/core/Grid';
 
 function Home() {
   return (
-    <div>
+    <Grid container direction="column" className="mainGrid">
       <Header />
       <TinderCards />
       <SwipeButtons />
-    </div>
+    </Grid>
   );
 }
 
